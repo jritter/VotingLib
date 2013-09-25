e29796f88588b138e8283f4a5879979aff33f252
Juerg Ritter <juerg_ritter@bluewin.ch>
2013-09-25 21:06:45 +0200

    added fragment for listing networks

---
+++ VotingLib/src/ch/bfh/evoting/votinglib/fragment/NetworkArrayAdapter.java
@@ -0,0 +1,104 @@
+/*
+ *  UniCrypt Cryptographic Library
+ *  Copyright (c) 2013 Berner Fachhochschule, Biel, Switzerland.
+ *  All rights reserved.
+ *
+ *  Distributable under GPL license.
+ *  See terms of license at gnu.org.
+ *  
+ */
+
+package ch.bfh.evoting.votinglib.fragment;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import ch.bfh.evoting.votinglib.R;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Implements an ArrayAdapter which maps the values of the Hashmaps stored in
+ * the ArrayList to the views in the layout displaying all the available WLAN
+ * networks
+ * 
+ * @author Juerg Ritter (rittj1@bfh.ch)
+ */
+public class NetworkArrayAdapter extends ArrayAdapter<HashMap<String, Object>> {
+
+	private ArrayList<HashMap<String, Object>> items;
+	private Context context;
+	private String capabilities;
+
+	/**
+	 * @param context
+	 *            The context from which it has been created
+	 * @param textViewResourceId
+	 *            The id of the item layout
+	 * @param items
+	 *            The ArrayList with the values
+	 */
+	public NetworkArrayAdapter(Context context, int textViewResourceId,
+			ArrayList<HashMap<String, Object>> items) {
+		super(context, textViewResourceId, items);
+		this.context = context;
+		this.items = items;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.widget.ArrayAdapter#getView(int, android.view.View,
+	 * android.view.ViewGroup)
+	 */
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View view = convertView;
+		if (view == null) {
+			LayoutInflater inflater = (LayoutInflater) context
+					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			view = inflater.inflate(R.layout.list_item_network, null);
+		}
+
+		// extract the Hashmap at the according postition of the ArrayList
+		HashMap<String, Object> item = items.get(position);
+		if (item != null) {
+			// extract the views in the layout
+			TextView content = (TextView) view.findViewById(R.id.content);
+			TextView description = (TextView) view
+					.findViewById(R.id.description);
+			ImageView icon = (ImageView) view.findViewById(R.id.icon);
+
+			if (this.getCount() - 1 == position) {
+				// the last item is for the "Create Network" item, so the icon
+				// needs to be the router
+				icon.setImageResource(R.drawable.glyphicons_046_router);
+				icon.setBackgroundColor(context.getResources().getColor(
+						android.R.color.holo_purple));
+				description.setText("");
+			} else {
+				// set the values of the labels accordingly
+				icon.setImageResource(R.drawable.glyphicons_032_wifi_alt);
+				icon.setBackgroundColor(context.getResources().getColor(
+						android.R.color.holo_blue_light));
+				capabilities = (String) item.get("capabilities");
+				if (capabilities == null) {
+					description.setText("");
+				} else if (capabilities.contains("WPA")) {
+					description.setText("WPA secured network");
+				} else if (capabilities.contains("WEP")) {
+					description.setText("WEP secured network");
+				} else {
+					description.setText("unsecure open network");
+				}
+			}
+			content.setText((String) item.get("SSID"));
+
+		}
+		return view;
+	}
+}
---
+++ VotingLib/src/ch/bfh/evoting/votinglib/fragment/NetworkListFragment.java
@@ -0,0 +1,293 @@
+package ch.bfh.evoting.votinglib.fragment;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import android.app.Activity;
+import android.app.DialogFragment;
+import android.app.ListFragment;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
+import android.nfc.NfcAdapter;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.AdapterView.OnItemClickListener;
+import ch.bfh.evoting.instacirclelib.wifi.AdhocWifiManager;
+import ch.bfh.evoting.votinglib.R;
+
+/**
+ * This class implements the fragment which lists all the messages of the
+ * conversation (left tab of the NetworkActiveActivity)
+ * 
+ * @author Juerg Ritter (rittj1@bfh.ch)
+ */
+public class NetworkListFragment extends ListFragment implements OnItemClickListener {
+
+	private static final String TAG = NetworkListFragment.class.getSimpleName();
+
+	private static final String STATE_ACTIVATED_POSITION = "activated_position";
+
+	private int mActivatedPosition = ListView.INVALID_POSITION;
+
+	private NetworkArrayAdapter adapter;
+
+	private ArrayList<HashMap<String, Object>> arraylist = new ArrayList<HashMap<String, Object>>();
+	private HashMap<String, Object> lastItem = new HashMap<String, Object>();
+
+	private ListView lv;
+
+	private SharedPreferences preferences;
+	private List<ScanResult> results;
+	private List<WifiConfiguration> configuredNetworks;
+
+	private ScanResult selectedResult;
+
+	private EditText txtIdentification;
+
+	private WifiManager wifi;
+	private AdhocWifiManager adhoc;
+
+	private BroadcastReceiver wifibroadcastreceiver;
+	private NfcAdapter nfcAdapter;
+	private PendingIntent pendingIntent;
+	private IntentFilter nfcIntentFilter;
+	private IntentFilter[] intentFiltersArray;
+	private boolean nfcAvailable;
+	private Parcelable[] rawMsgs;
+	private int selectedNetId;
+	
+	private DialogFragment dialogFragment;
+	public static final int DIALOG_FRAGMENT = 1;
+
+	public interface Callbacks {
+		public void onItemSelected(String id);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.support.v4.app.Fragment#onCreate(android.os.Bundle)
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.support.v4.app.ListFragment#onViewCreated(android.view.View,
+	 * android.os.Bundle)
+	 */
+	@Override
+	public void onViewCreated(View view, Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+		if (savedInstanceState != null
+				&& savedInstanceState.containsKey(STATE_ACTIVATED_POSITION)) {
+			setActivatedPosition(savedInstanceState
+					.getInt(STATE_ACTIVATED_POSITION));
+		}
+
+		this.getListView().setTranscriptMode(2);
+
+		// initializing the adapter and assign it to myself
+		// adapter = new NetworkArrayAdapter(getActivity(), cursor);
+		setListAdapter(adapter);
+
+		super.onCreate(savedInstanceState);
+
+		lv = (ListView) this.getListView();
+		
+		// Handling the WiFi
+		wifi = (WifiManager) getActivity().getSystemService(
+				Context.WIFI_SERVICE);
+		wifi.startScan();
+		adhoc = new AdhocWifiManager(wifi);
+
+		lastItem.put("SSID", "Create new network...");
+
+		adapter = new NetworkArrayAdapter(getActivity(),
+				R.layout.list_item_network, arraylist);
+		adapter.add(lastItem);
+		lv.setAdapter(adapter);
+		lv.setOnItemClickListener(this);
+
+		// defining what happens as soon as scan results arrive
+		wifibroadcastreceiver = new BroadcastReceiver() {
+
+			@Override
+			public void onReceive(Context c, Intent intent) {
+				results = wifi.getScanResults();
+				configuredNetworks = wifi.getConfiguredNetworks();
+				arraylist.clear();
+
+				for (ScanResult result : results) {
+					HashMap<String, Object> item = new HashMap<String, Object>();
+
+					item.put("known", false);
+
+					// check whether the network is already known, i.e. the
+					// password is already stored in the device
+					for (WifiConfiguration configuredNetwork : configuredNetworks) {
+						if (configuredNetwork.SSID.equals("\"".concat(
+								result.SSID).concat("\""))) {
+							item.put("known", true);
+							item.put("netid", configuredNetwork.networkId);
+							break;
+						}
+					}
+
+					if (result.capabilities.contains("WPA")
+							|| result.capabilities.contains("WEP")) {
+						item.put("secure", true);
+					} else {
+						item.put("secure", false);
+					}
+					item.put("SSID", result.SSID);
+					item.put("capabilities", result.capabilities);
+					item.put("object", result);
+					arraylist.add(item);
+					Log.d(TAG, result.SSID + " known: " + item.get("known")
+							+ " netid " + item.get("netid"));
+				}
+				arraylist.add(lastItem);
+				adapter.notifyDataSetChanged();
+
+			}
+		};
+
+		// register the receiver, subscribing for the SCAN_RESULTS_AVAILABLE
+		// action
+		getActivity().registerReceiver(wifibroadcastreceiver,
+				new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * android.support.v4.app.Fragment#onSaveInstanceState(android.os.Bundle)
+	 */
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		if (mActivatedPosition != ListView.INVALID_POSITION) {
+			outState.putInt(STATE_ACTIVATED_POSITION, mActivatedPosition);
+		}
+	}
+
+	/**
+	 * @param activateOnItemClick
+	 */
+	public void setActivateOnItemClick(boolean activateOnItemClick) {
+		getListView().setChoiceMode(
+				activateOnItemClick ? ListView.CHOICE_MODE_SINGLE
+						: ListView.CHOICE_MODE_NONE);
+	}
+
+	/**
+	 * @param position
+	 */
+	public void setActivatedPosition(int position) {
+		if (position == ListView.INVALID_POSITION) {
+			getListView().setItemChecked(mActivatedPosition, false);
+		} else {
+			getListView().setItemChecked(position, true);
+		}
+
+		mActivatedPosition = position;
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * android.widget.AdapterView.OnItemClickListener#onItemClick(android.widget
+	 * .AdapterView, android.view.View, int, long)
+	 */
+	public void onItemClick(AdapterView<?> listview, View view, int position,
+			long id) {
+
+		if (listview.getAdapter().getCount() - 1 == position) {
+			// handling the last item in the list, which is the "Create network"
+			// item
+			//Intent intent = new Intent(this, CreateNetworkActivity.class);
+			//startActivity(intent);
+		} else {
+			// extract the Hashmap assigned to the position which has been
+			// clicked
+			HashMap<String, Object> hash = (HashMap<String, Object>) listview
+					.getAdapter().getItem(position);
+
+			selectedResult = (ScanResult) hash.get("object");
+			selectedNetId = -1;
+
+			// going through the different connection scenarios
+			
+			if ((Boolean) hash.get("secure") && !((Boolean) hash.get("known"))) {
+				dialogFragment = new ConnectNetworkDialogFragment(true);
+			} else if ((Boolean) hash.get("known")) {
+				selectedNetId = (Integer) hash.get("netid");
+				dialogFragment = new ConnectNetworkDialogFragment(false);
+			} else {
+				dialogFragment = new ConnectNetworkDialogFragment(false);
+			}
+			dialogFragment.setTargetFragment(this, DIALOG_FRAGMENT);
+			dialogFragment.show(getFragmentManager(), TAG);
+
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.support.v4.app.Fragment#onDestroy()
+	 */
+	@Override
+	public void onDestroy() {
+		// Unregister since the activity is about to be closed.
+		getActivity().unregisterReceiver(wifibroadcastreceiver);
+		super.onDestroy();
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		switch(requestCode){
+		
+		case DIALOG_FRAGMENT:
+
+            if (resultCode == Activity.RESULT_OK) {
+            	if (selectedNetId != -1) {
+        			adhoc.connectToNetwork(selectedNetId, getActivity());
+        		} else {
+        			adhoc.connectToNetwork(selectedResult.SSID,
+        					((ConnectNetworkDialogFragment) dialogFragment).getNetworkKey(),
+        					getActivity());
+        		}
+
+            	dialogFragment.dismiss();
+            } else if (resultCode == Activity.RESULT_CANCELED){
+            	dialogFragment.dismiss();
+            }
+
+            break;
+		}
+	}
+	
+	
+
+}
